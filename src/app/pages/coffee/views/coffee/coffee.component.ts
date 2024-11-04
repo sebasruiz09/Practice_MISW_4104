a2dbe68f1dc40b833f479f238ce968273ef9d834
sebasruiz09 <sebasruiz909@gmail.com>
2024-11-03 22:15:16 -0500

    FEAT : add total coffee types function and show info in template

--- src/app/pages/coffee/views/coffee/coffee.component.ts
+++ src/app/pages/coffee/views/coffee/coffee.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from '@angular/common';
+import { KeyValuePipe, NgFor } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { HttpService } from '@shared/services/http.service';
 import { Coffee } from '@shared/types/coffe.type';
@@ -6,7 +6,7 @@ import { Coffee } from '@shared/types/coffe.type';
 @Component({
   selector: 'app-coffee',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, KeyValuePipe],
   templateUrl: './coffee.component.html',
   styleUrls: ['./coffee.component.scss'],
 })
@@ -14,13 +14,14 @@ export default class CoffeeComponent implements OnInit {
   private readonly _httpService: HttpService = inject(HttpService);
 
   public data = signal<Coffee[]>([]);
-  public headers = signal<String[]>([]);
+  public coffeTypes: { [key: string]: number } = {};
 
   ngOnInit(): void {
-    this._httpService
-      .getCoffeList()
-      .subscribe(
-        (data) => (this.data.set(data), this.headers.set(Object.keys(data[0]))),
-      );
+    this._httpService.getCoffeList().subscribe((data) => {
+      this.data.set(data);
+      data.forEach((coffee) => {
+        this.coffeTypes[coffee.tipo] = (this.coffeTypes[coffee.tipo] || 0) + 1;
+      });
+    });
   }
 }
